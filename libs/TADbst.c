b54b0f70abfcd98e21000268ef237b204b13f75a
Lucas Soares <lucas.s.gomes@ufv.br>
2019-03-17 16:37:28 -0300

    Comentários

--- libs/TADbst.c
+++ libs/TADbst.c
@@ -44,42 +44,73 @@ void insereTAD(tipoApontador *no, int info){
 }
 
 /**
- *
- * @param no
- * @param info
+ * Função que exibe mensagem de encontrado ou não encontrado o valor passado por parâmetro
+ * @param no Endereço do apontador para esse no
+ * @param info Informação a ser pesquisada
  */
 void pesquisaTAD(tipoApontador *no, int info){
+    //Novamente a função é recursiva, então assim que chegar ao último nó da árvore ele será null e
+    //a função retornará de onde foi chamada.
+    //Verifico se o nó é null
     if(*no == NULL){
         printf("Erro: Valor não encontrado!\n");
         return;
     }
+    //Verifico se a informação é menor do que a informação que está no nó.
+    //Em caso positivo, a pesquisa deve continuar pelo lado esquerdo da árvore.
     if(info < (*no)->chave){
         pesquisaTAD(&(*no)->left, info);
     }
+    //Verifico se a informação é menor do que a informação que está no nó.
+    //Em caso positivo, a pesquisa deve continuar pelo lado direito da árvore ou ela já terá chegado no fim e encontrado o valor.
     if(info > (*no)->chave){
         pesquisaTAD(&(*no)->left, info);
     } else {
+        printf("Passei pelos nó: %d\n", (*no)->chave);
         printf("Encontrado!");
     }
 }
 
+/**
+ * Função que exibe de forma ordenada os valores da árvore
+ * @param no Endereço do apontador para esse no
+ */
 void ordem(tipoApontador no){
+    //Se não ouver dado no nó devo parar a execução
     if(no == NULL) return;
+    //A exibição em ordem começa pelo menor(esquerda)
     ordem(no->left);
-    printf("%ld\n", no->chave);
+    printf("%ld\n", no->chave); //Mostro o menor valor possível dessa execução
+    //Chamo o nó maior daquela execução
     ordem(no->right);
 }
 
+/**
+ * Função que exibe de forma pré-ordenada os valores da árvore
+ * @param no Endereço do apontador para esse no
+ */
 void preordem(tipoApontador no){
+    //Se não ouver dado no nó devo parar a execução
     if(no == NULL) return;
-    printf("%ld\n", no->chave);
+
+    printf("%ld\n", no->chave); //Mostro o valor da raíz
+    //A exibição em pré-ordem continua pelo menor(esquerda)
     preordem(no->left);
+    //A exibição em pré-ordem chama os valores a direita naquela execução
     preordem(no->right);
 }
 
+/**
+ * Função que exibe de forma pós-ordenada os valores da árvore
+ * Endereço do apontador para esse no
+ * @param no
+ */
 void posordem(tipoApontador no){
+    //Se não ouver dado no nó devo parar a execução
     if(no == NULL) return;
+    //A exibição em pos-ordem começa pelo menor(esquerda)
     posordem(no->left);
+    //A exibição em pos-ordem chama os valores a direita naquela execução
     posordem(no->right);
-    printf("%ld\n", no->chave);
+    printf("%ld\n", no->chave); //Mostro o valor da raíz
 }
