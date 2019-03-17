80d1e64159b293ecad8649e1b9f8f41fff9caf08
Lucas Soares <lucas.s.gomes@ufv.br>
2019-03-17 16:27:23 -0300

    Comentário da função insere

--- libs/TADbst.c
+++ libs/TADbst.c
@@ -1,25 +1,41 @@
 #include <wchar.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "TADbst.h"
+#include "TADbst.h" //Inclusão do cabeçalho
 
+/**
+ * Nessa função eu aponto o ponteiro da estrutura no para NULL
+ * @param no Endereço do apontador para esse no
+ */
 void inicializaTAD(tipoApontador *no){
     *no = NULL;
 }
 
+/**
+ * Nessa função eu insiro a informação (int) em um nó da árvore seguindo a definição de árvores do tipo BST
+ * @param no Endereço do apontador para esse no
+ * @param info Informação a ser adicionada
+ */
 void insereTAD(tipoApontador *no, int info){
+    //Se o nó é nulo posso alocar o espaço de um nó e apontar a esquerda e a direira para nulo,
+    //inserindo também a informação recolhida na função
     if(*no == NULL){
-        *no = (tipoApontador)malloc(sizeof(tipoNo));
-        (*no)->chave = info;
-        (*no)->left = NULL;
-        (*no)->right = NULL;
-        return;
+        *no = (tipoApontador)malloc(sizeof(tipoNo)); //Alocando espaço
+        (*no)->chave = info; //Armazenando
+        (*no)->left = NULL; //Apontando esquerda para null
+        (*no)->right = NULL; //Apontando direita para null
+        return; //retorno a função com nada já que ela é void
     }
+    //A função é recursiva, então sempre que quero inserir um dado eu passo pelas mesmas verificações
+    //A verificação abaixo faz com que se a informação for menor ou igual ao que já tenho no nó raiz
+    //posso chamar a função recursivamente e inserir a informação no nó a esquerda
     if(info <= (*no)->chave){
         insereTAD(&(*no)->left, info);
         printf("\nPreenchido a esquerda, ou seja, menor do que a raíz do nó anterior que é o %d\n", (*no)->chave);
         return;
     }
+    //A verificação abaixo faz com que se a informação for maior ao que já tenho no nó raiz
+    //posso chamar a função recursivamente e inserir a informação no nó a direita
     if(info > (*no)->chave){
         insereTAD(&(*no)->right, info);
         printf("\nPreenchido a direita, ou seja, maior do que a raíz do nó anterior que é o  %d\n", (*no)->chave);
@@ -27,6 +43,11 @@ void insereTAD(tipoApontador *no, int info){
     }
 }
 
+/**
+ *
+ * @param no
+ * @param info
+ */
 void pesquisaTAD(tipoApontador *no, int info){
     if(*no == NULL){
         printf("Erro: Valor não encontrado!\n");
--- libs/TADbst.h
+++ libs/TADbst.h
@@ -1,26 +1,54 @@
-//
-// Created by lucassgomes on 16/03/19.
-//
-
+/**
+ * Criado em 17/01/2019 por Lucas Soares
+ * AEDSII - Gláucia Braga e Silva
+ */
+ //Definições do CLION
 #ifndef BST_TADBST_H
 #define BST_TADBST_H
 
 #endif //BST_TADBST_H
 
+// Definindo a estrutura de dados para a árvore, ou seja, o nó
 typedef struct tipoNo * tipoApontador;
 typedef struct tipoNo {
     int chave;
     tipoApontador left, right;
 } tipoNo;
 
+/**
+ * Função que inicializa movendo um ponteiro de um nó (tipoNo) qualquer para NULL
+ * @param no Endereço do apontador para esse nó
+ */
 void inicializaTAD(tipoApontador *no);
 
+/**
+ * Função que insere valores (int) no nó da árvore
+ * @param no Endereço do apontador para esse no
+ * @param info Informação a ser inserida
+ */
 void insereTAD(tipoApontador *no, int info);
 
+/**
+ * Função que pesquisa um valor e retorna mensagem caso encontre o mesmo
+ * @param no Endereço do apontador para esse no
+ * @param info Informação a ser inserida
+ */
 void pesquisaTAD(tipoApontador *no, int info);
 
+/**
+ * Função que exibe a árvore de forma ordenada
+ * @param no Endereço do apontador para esse no
+ */
 void ordem(tipoApontador no);
 
+/**
+ * Função que exibe a árvore de forma pré-ordenada
+ * @param no Endereço do apontador para esse no
+ */
 void preordem(tipoApontador no);
 
+/**
+ * Função que exibe a árvore de forma pós-ordenada
+ * @param no
+ */
 void posordem(tipoApontador no);
